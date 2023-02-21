d00466349c58ca62c38ac6d2831252d4df0c20d7
r677666 <omari.gaskins.dev@gmail.com>
2023-02-20 19:30:09 -0500

    updates to the upgrade file

--- src/Upgrade.js
+++ src/Upgrade.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row, Card, CardGroup, Navbar, Image, Modal } from 'react-bootstrap';
+import { Container, InputGroup, FormControl, Button, Row, Card, CardGroup, Navbar, Image, Modal, Col } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import Navigation from './Navigation';
 
@@ -8,12 +8,14 @@ export default function Upgrade(){
     return(
         <div>
             <Navigation/>
-            <h1 style={{marginTop:"8rem"}}> UPGRADE TO PRO </h1>
-            <div style={{maxWidth:"40rem"}}>
-                <CardGroup>
-                <Card style={{width:'15rem',height:'25rem', paddingTop:'1rem' }}> Free </Card>
-                <Card style={{width:'15rem',height:'25rem', paddingTop:'1rem' }}> PRO </Card>
-                </CardGroup>
+            <h1 style={{marginTop:"8rem",textAlign:"center"}}> UPGRADE TO PRO </h1>
+            <div>
+            <Container className='d-flex justify-content-center text-center'>
+                {/* <CardGroup style={{textAlign:"center"}}> */}
+                    <Card style={{width:'23rem',height:'32rem', paddingTop:'1rem', marginRight:'1rem' }}> Free </Card>
+                    <Card style={{width:'23rem',height:'32rem', paddingTop:'1rem' }}> PRO </Card>
+                {/* </CardGroup> */}
+            </Container>
             </div>
         </div>
     )
